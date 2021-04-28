a5eb22164d9449acd642d398e6993ed7d70d8e36
Diego Polo <80282895+Colerico02@users.noreply.github.com>
2021-04-28 09:00:31 -0500

    Diseño login

--- js/main.js
+++ js/main.js
@@ -1,94 +1,17 @@
-<<<<<<< HEAD
-let username = document.querySelector(".username"); 
-let password = document.querySelector(".password"); 
-let btnLogin = document.querySelector(".login__boton"); 
-let inform = document.querySelector(".informacion");
-
-let usernameCheck = "Bret"
-
-
-
-btnLogin.addEventListener('click', ()=>{
-   username.style.border = "1px solid black"
-   password.style.border = "1px solid black"
-   let alertText = ""; 
-
-  if(username.value.length == 0 && password.value.length == 0)
-  {
-   username.style.border = "1px solid red";
-   password.style.border = "1px solid red";
-   alertText = `<p>Los campos están vacios</p>`
-  }
-  else if(username.value.length == 0)
-  { username.style.border = "1px solid red";
-    alertText = `<p>EL username está vacio</p>`
-  }
-  else if(password.value.length == 0) 
-  {password.style.border = "1px solid red";
-  alertText = `<p>La contraseña está vacia</p>`
-  }
-  else if(username.value == usernameCheck  && password.value == "1234")
-  {
-   username.style.border = "1px solid green";
-   password.style.border = "1px solid green";
-   GetDataApi(usernameCheck); 
-
-
-  }  
-  else if(username.value != usernameCheck  && password.value != "1234")
-  {
-   username.style.border = "1px solid red";
-   password.style.border = "1px solid red";
-   alertText = `<p>Los datos son incorrectos</p>`
-  } 
-  else if(username.value != usernameCheck )
-  {
-  username.style.border = "1px solid red";
-  password.style.border = "1px solid red";
-  alertText = `<p>El nombre y/o contraseña son incorrectos</p>`
-
-  }
-  else if(password.value != usernameCheck ) 
-  {
-     password.style.border = "1px solid red";
-     alertText = `<p>La contraseña es erronea</p>`
-
-
-  }  
-   inform.innerHTML = alertText; 
-
-})
-
-const GetDataApi = (nombre)=>{
-
-    fetch("https://jsonplaceholder.typicode.com/users")
-   .then(response => response.json() )
-   .then(datos =>{
-    if(datos){
-         location.href = "dashboard.html";
-         alert(datos[0].name)
-
-      }
-    
-    })
-
-
- 
-
-=======
 // Variables globales
-var username = document.querySelector(".username");
-var password = document.querySelector(".password");
+var username = document.querySelector(".login__username");
+var password = document.querySelector(".login__password");
 var btnLogin = document.querySelector(".login__boton");
 var inform = document.querySelector(".informacion");
+var contentLogin = document.querySelectorAll(".login__view"); 
 
 // Capturar evento click del boton
 btnLogin.addEventListener("click", validationForm);
 
 //Funcion de validación
 function validationForm() {
-  username.style.border = "1px solid black";
-  password.style.border = "1px solid black";
+  username.style.border = "1px solid rgb(26, 26, 26)";
+  password.style.border = "1px solid rgb(26, 26, 26)";
   let alertText = "";
 
   if (username.value.length == 0 && password.value.length == 0) {
@@ -108,6 +31,7 @@ function validationForm() {
    //   Logeo correcto
     username.style.border = "1px solid green";
     password.style.border = "1px solid green";
+    for(let i = 0; i<contentLogin.length; i++)contentLogin[i].style.display = "none"; 
     obtencionDatos();
   } else if (username.value != "Diego" && password.value != "1234") {
    //   Si los datos son incorrectos
@@ -137,5 +61,4 @@ async function obtencionDatos() {
                 <p><b>Email: </b>${users.email}</p>
    `;
    inform.innerHTML = alertText;
->>>>>>> 8cb219e4ef198f18fcad1dcac4717cd9fd2c58d9
 }
