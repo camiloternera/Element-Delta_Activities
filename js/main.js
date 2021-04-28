fb688a4d65d7d2a7fa524cc5db0fc1d91323b3ad
Diego Polo <80282895+Colerico02@users.noreply.github.com>
2021-04-28 07:30:14 -0500

    PRUEBA

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-var username = document.querySelector(".username"); 
-var password = document.querySelector(".password"); 
-var btnLogin = document.querySelector(".login__boton"); 
-var inform = document.querySelector(".informacion");
+let username = document.querySelector(".username"); 
+let password = document.querySelector(".password"); 
+let btnLogin = document.querySelector(".login__boton"); 
+let inform = document.querySelector(".informacion");
 
+let usernameCheck = "Bret"
 
 
 
 btnLogin.addEventListener('click', ()=>{
    username.style.border = "1px solid black"
    password.style.border = "1px solid black"
-   let alertText; 
+   let alertText = ""; 
 
   if(username.value.length == 0 && password.value.length == 0)
   {
@@ -25,35 +26,52 @@ btnLogin.addEventListener('click', ()=>{
   {password.style.border = "1px solid red";
   alertText = `<p>La contraseña está vacia</p>`
   }
-  else if(username.value == "Diego" && password.value == "1234")
+  else if(username.value == usernameCheck  && password.value == "1234")
   {
    username.style.border = "1px solid green";
    password.style.border = "1px solid green";
-   alertText = `<p>Usuario validado</p>`
+   GetDataApi(usernameCheck); 
+
 
   }  
-  else if(username.value != "Diego" && password.value != "1234")
+  else if(username.value != usernameCheck  && password.value != "1234")
   {
    username.style.border = "1px solid red";
    password.style.border = "1px solid red";
    alertText = `<p>Los datos son incorrectos</p>`
   } 
-  else if(username.value != "Diego")
+  else if(username.value != usernameCheck )
   {
   username.style.border = "1px solid red";
   password.style.border = "1px solid red";
   alertText = `<p>El nombre y/o contraseña son incorrectos</p>`
 
   }
-  else if(password.value != "1234") 
+  else if(password.value != usernameCheck ) 
   {
      password.style.border = "1px solid red";
      alertText = `<p>La contraseña es erronea</p>`
 
 
   }  
-
- 
    inform.innerHTML = alertText; 
 
 })
+
+const GetDataApi = (nombre)=>{
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+   .then(response => response.json() )
+   .then(datos =>{
+    if(datos){
+         location.href = "dashboard.html";
+         alert(datos[0].name)
+
+      }
+    
+    })
+
+
+ 
+
+}
